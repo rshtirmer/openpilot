58b841160d0f9ecfac4509392fa23f9fbe6d2a33
Ryan Shtirmer <rshtirmer@gmail.com>
2019-05-22 22:33:27 -0400

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -185,7 +185,7 @@ class CarInterface(object):
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.3], [0.05]]
       ret.lateralTuning.pid.kf = 0.00007818594
 
-    elif candidate == CAR.LEXUS_GS300H:
+    elif candidate == CAR.LEXUS_IS_2019:
       stop_and_go = False
       ret.safetyParam = 100
       ret.wheelbase = 2.79908
